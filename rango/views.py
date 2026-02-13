2b21b5a6030146c16b1d78a956eaa3a0feb52b26
Erin McElvanna <3027147M@student.gla.ac.uk>
2026-02-13 12:22:50 +0000

    completed chapter 8

--- rango/views.py
+++ rango/views.py
@@ -10,6 +10,8 @@ def index(request):
     page_list = Page.objects.order_by('-views')[:5]
     context_dict={'categories':category_list, 'pages':page_list,}
     return render(request, 'rango/index.html',context=context_dict)
+def about(request):
+    return render(request, 'rango/about.html')
 
 def show_category(request, category_name_slug):
     context_dict = {}
